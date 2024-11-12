c7834797e3a402b30305e69e8e9f0653f83d0fe2
Methara Sansiluni <metharasansiluni2003@gmail.com>
2024-11-12 14:49:04 +0530

    Changed the onclick to a function in Js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+//palindrome checker function
 
-const  checkBtn = document.getElementById("checkBtn");
+ function check(){
 
-checkBtn.addEventListener("click", function (){
-
-    const textBox = document.getElementById("textBox").value,
-    result = document.getElementById("result");
+    const textBox = document.getElementById("textBox").value;
+    const result = document.getElementById("result");
 
     let changedWord = textBox.replace(/[^A-Za-z0-9]/g, "");
     console.log(changedWord);
     let reverseWord = [...changedWord].reverse().join("");
     console.log(reverseWord);
 
-    if(changedWord.toLowerCase() === reverseWord.toLowerCase()){
+    if(textBox === ""){
+        result.textContent = `Please enter a word or sentence`
+    }
+    else if(changedWord.toLowerCase() === reverseWord.toLowerCase()){
         result.textContent = `"${textBox}" is a Palindrome.`
     }
     else{
         result.textContent = `"${textBox}" is not a Palindrome.`
     }
 
-})
+}
